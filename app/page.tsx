f3ec40309d7b4a462f1446bf4db6753b02f9f074
Lincy David <lincy.d@fayausa.com>
2024-02-27 13:12:34 +0530

    feat: add code to view dev finder in github

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faLink,
+  faMapMarker,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-export default function Home() {
+const HomePage = () => {
+  interface User {
+    login: string;
+    name: string;
+    bio: string;
+    id: number;
+    avatar_url: string;
+    public_repos: string;
+    followers: string;
+    following: string;
+    location: string;
+    twitter_username: string;
+    url: string;
+    html_url: string;
+  }
+
+  let url = "";
+  const [user, setUser] = useState<User | null>(null);
+  const [textInput, setTextInput] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextInput(e.target.value);
+  };
+
+  const handleButtonClick: React.MouseEventHandler<HTMLButtonElement> = async (
+    event
+  ) => {
+    url = "https://api.github.com/users/" + textInput;
+    event.preventDefault();
+    await fetchUser();
+  };
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(url);
+      setUser(res.data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen flex justify-center items-center bg-customBackground">
+      <div className="flex flex-col h-full">
+        <div className="rounded p-2 m-2 justify-center items-center flex">
+          <input
+            className="px-4 py-2 rounded-lg border border-gray-300 focus:border-blue-500 bg-customeDiv text-white placeholder-gray-400"
+            type="text"
+            value={textInput}
+            onChange={handleInputChange}
+            placeholder="Search"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleButtonClick}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Search
+          </button>
+        </div>
+        <div className="bg-customeDiv p-8 rounded-lg text-white">
+          {user && (
+            <div>
+              <div className="flex">
+                <div className="w-1/2 m-5">
+                  <Image
+                    alt={"User Image"}
+                    width={150}
+                    height={150}
+                    className="profile-pic border-4 rounded-full border-solid border-[#5f78a2]"
+                    src={user?.avatar_url ? user?.avatar_url : "/images.png"}
+                  />
+                </div>
+                <div className="w-1/2 m-5">
+                  <p>{user.name}</p>
+                  <p className="text-blue-500 font-bold">{user.id}</p>
+                  <p>{user.bio}</p>
+                </div>
+              </div>
+              <div className="flex bg-customBackground p-2 text-white mb-2">
+                <div className="w-1/2">
+                  <label>Repos</label>
+                  <p>{user.public_repos ? user.public_repos : "-"}</p>
+                </div>
+                <div className="w-1/2">
+                  <label>Followers</label>
+                  <p>{user.followers}</p>
+                </div>
+                <div className="w-1/2">
+                  <label>Following</label>
+                  <p>{user.followers}</p>
+                </div>
+              </div>
+              <div className="flex p-2 text-white mb-2">
+                <div className="w-1/2">
+                  <FontAwesomeIcon icon={faMapMarker} className="text-white" />
+                  <label className="m-1 text-sm">
+                    {user.location ? user.location : "Not Available"}
+                  </label>
+                </div>
+                <div className="w-1/2 text-right">
+                  <FontAwesomeIcon icon={faTwitter} className="text-white" />
+                  <label className="m-1 text-sm">
+                    {user.twitter_username ? user.twitter_username : "Not Available"}
+                  </label>
+                </div>
+              </div>
+              <div className="flex p-2 text-white mb-2">
+                <div className="w-1/2">
+                  <FontAwesomeIcon icon={faLink} className="text-white" />
+                  <label className="m-1 text-sm">
+                    {user.html_url ? user.html_url : "Not Available"}
+                  </label>
+                </div>
+                <div className="flex w-1/2 text-right">
+                  <FontAwesomeIcon icon={faBuilding} className="text-white" />
+                  <label className="m-1 text-sm">
+                    {user.url ? user.url : "Not Available"}
+                  </label>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default HomePage;
---
+++ fontawesome.config.js
@@ -0,0 +1,6 @@
+// fontawesome.config.js
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faBuilding, faLink, faMapMarker } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter, faBuilding, faLink, faMapMarker);
